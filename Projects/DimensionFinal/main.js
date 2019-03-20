1be13b06f62702740b990398c080ae23bb53f509
juliaehlert <juliaehlert1@gmail.com>
2019-03-20 09:30:43 -0700

    Dimension Final
    final version of dimension before deadline

---
+++ Projects/DimensionFinal/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+  var intro = $(".intro");
+
+  intro.next().hide();
+
+  intro.click(function () {
+    $(this).next().show();
+    $(this).hide();
+  })
+
+
+
+
+
+  // var vid = document.getElementById("myVideo");
+  //
+  // function playVid() {
+  //   vid.play();
+  // }
+  //
+  // function pauseVid() {
+  //   vid.pause();
+  // }
+
+  //
+  //
+  // $("#p1").click(function() {
+  //   $("#p2").show();
+  //   $("#p1").hide();
+  // });
+  //
+  // $("#p2").click(function() {
+  //   $("#p3").show();
+  //   $("#p2").hide();
+  //
+  // });
+  //
+  // $("#p3").click(function() {
+  //   $("#p4").show();
+  // });
+  //
+  // $('#p4').click(function() {
+  //   $('p').hide();
+  // })
+
+});
